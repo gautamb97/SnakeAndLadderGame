45b6f85416af2ca0fdaa6b33941b093483e6d66f
gautamb97 <www.gautam971997@gmail.com>
2021-02-12 20:44:03 +0530

    Refactored:Divided all UCs in methods

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,25 +3,23 @@ public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder Game");
-		 Player choice = new Player();
-	     choice.chooseOption();
+		Player playing = new Player();
+	     playing.chooseOption();
+	     playing.ensuringExactWinningPosition();
 	}
 
 }
 
 class Player{
-    int playerPosition = 0;
-    private final int winning_point = 100;
+	int playerPosition = 0;
+    double die;
+    private static final int winningPoint = 100;
     
-    double die = 1+(Math.floor(Math.random() * 10 ) % 6);
-    
+    public void dieValueGeneration() {
+    	die = 1+(Math.floor(Math.random() * 10 ) % 6);
+    }
     public void chooseOption(){
-    	 int number_of_time_player_played=0;
-    	 while(playerPosition<winning_point) {
-    		 number_of_time_player_played++;
-             System.out.println("Number of times player played: "+number_of_time_player_played);
-             if (playerPosition < 0)
-                 playerPosition = 0;
+    	dieValueGeneration();
              int choose = (int) Math.floor(Math.random() * 10) % 3;
              switch (choose) {
                  case 0:
@@ -36,13 +34,21 @@ class Player{
                      playerPosition -= die;
                      break;
              }
-             System.out.println("Die Value: "+die);
-             if(playerPosition>winning_point)
-                 playerPosition =winning_point;
-             System.out.println("Player position " + playerPosition);
-         }
-    	 
-         System.out.println("Hip....Hip...Hurrey....I won!!!");
      }
-    
+    public void repeatingTillTheWinningPosition() {
+    	int numberOfTimePlayerPlayed=0;
+    	while(playerPosition < winningPoint ) {
+    		numberOfTimePlayerPlayed++;
+            System.out.println("Number of times player played: "+ numberOfTimePlayerPlayed);
+            System.out.println("Die Value: "+die);
+    		chooseOption();
+    	}
+    }
+    public void ensuringExactWinningPosition() {
+    	repeatingTillTheWinningPosition();
+    	if(playerPosition> winningPoint)
+         playerPosition = winningPoint;
+    	System.out.println("Player position " + playerPosition);
+    	System.out.println("Hip....Hip...Hurrey....I won!!!");
+    }  
 }
